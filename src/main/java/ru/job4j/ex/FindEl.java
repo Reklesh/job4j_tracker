40d19c2620fad69f46a85f75583411f748d6266b
Acer <a-krickiy@mail.ru>
2023-06-17 23:02:32 +0300

    0.4. Пользовательские исключения. [#219367 # [#219367]]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+import java.util.Objects;
+
+public class FindEl {
+
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (Objects.equals(value[index], key)) {
+                rsl = index;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Элемент отсутствует");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            indexOf(new String[]{"Первый", "Второй", "Третий"}, "Четвертый");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
