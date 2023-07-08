33f93cd52fe0f106c29e5a258234720802f66fa8
Acer <a-krickiy@mail.ru>
2023-07-08 10:25:40 +0300

    9. Изменить программу Tracker из 2-го модуля [#10039 # [#10039 #428567]]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -16,7 +16,7 @@ public class Tracker {
     }
 
     public List<Item> findAll() {
-        return items;
+        return new ArrayList<>(List.copyOf(items));
     }
 
     public List<Item> findByName(String key) {
